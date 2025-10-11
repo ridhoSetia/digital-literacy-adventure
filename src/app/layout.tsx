db7213d110dd48901b2ac552694e0d573d57566f
ridhoSetia <ridho.setiawan24406@gmail.com>
2025-10-11 17:50:49 +0800

    type: ganti type icon ke svg+xml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-        <link rel="icon" href="/logo.svg" type="image/x-icon"></link>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml"></link>
       </head>
       <body className={`${inter.variable} ${bebasNeue.variable} ${pressStart2P.variable} font-sans text-white bg-indigo-950`}>
         <AuthProvider>
